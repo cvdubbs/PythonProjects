c9f3c9e25e765bb0d4bee03267ed3643cc5c7fcb
cvdubbs <christophervanwert@gmail.com>
2021-06-29 23:05:54 -0400

    Updating @lists.py

--- Lists.py
+++ Lists.py
@@ -9,3 +9,25 @@ print(L)
 # Convert a string to a list
 L = list('abc')
 print(L)
+
+# Nested list example
+L = ['a', 'b', ['cc', 'dd', ['eee', 'fff']], 'g', 'h']
+print(L)
+
+# Print Third item in the third item
+print(L[2][2])
+
+# Print the first item in the third item in the thrid item
+print(L[2][2][0])
+
+# Sample List
+L = ['red', 'green', 'blue', 'yellow', 'black']
+
+# Print the first item in the list
+print(L[0])
+
+# Print the third item in the list
+print(L[2])
+
+# Print the last item in the list
+print(L[-1])
